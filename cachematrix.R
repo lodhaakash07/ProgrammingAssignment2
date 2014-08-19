82739b95fce6e1e5f6c22e3c16df45ae1dd62000
Akash Lodha <lodhaakash07@gmail.com>
2014-08-19 19:09:49 +0530

    Programming Assignment2 Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## R Programming
+## Coursera Specialization 2nd Course
 
 ## This function creates an enhanced matrix that contains
 ## functions for storing and retrieving the inverse of the matrix
 
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
+        ## setmatrix will set the matrix of type makeCacheMatrix and initialize its inverse
         setmatrix <- function(y) {
         x <<- y
         inv <<- NULL
         }
+        ## getmatrix will get the matrix of type makeCacheMatrix
+        
+        
         getmatrix <- function() x
+        
+        ## getinverse will get the inverse of the matrix
+        ## setinverse will set the inverse of the matrix
         setinverse <- function(inverse) inv <<- inverse
+        
+        # Gets the inverse of the matrix, inv_x
         getinverse <- function() inv
         list(setmatrix = setmatrix, getmatrix = getmatrix,
         setinverse = setinverse, getinverse = getinverse)
@@ -28,10 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
+        
+        # Check to see if the inverse of matrix is NOT NUL0L
+        # If it's NOT NULL, return inv_x as retrieved above and ends cacheSolve function
+        
         if(!is.null(inv)) {
         message("getting cached inverse of matrix")
         return(inv)
         }
+         # Inverse of matrix is NULL so calculate and set it
         inv <- solve(x$getmatrix(), ...)
         x$setinverse(inv)
         inv
